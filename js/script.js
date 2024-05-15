5800bb2c1da1a756996ec44b9b6e52075ed78055
José Dyego Henrique Cardoso de Sousa <116648398+dyegohrq@users.noreply.github.com>
2024-05-15 09:44:09 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 const buttonHeader = document.getElementById('button_header');
 const navbar = document.getElementById('navbar')
 
+const containerFaq = document.querySelectorAll('.container')
 buttonHeader.addEventListener('click', (e) => {
     e.preventDefault();
 
     buttonHeader.classList.toggle('open')
     navbar.classList.toggle('active')
+});
+
+containerFaq.forEach(container => {
+    container.addEventListener('click', () => {
+        container.classList.toggle('active')
+    })
 })
